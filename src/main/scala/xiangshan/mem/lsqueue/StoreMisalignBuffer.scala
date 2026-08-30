OpenXiangShan/XiangShan

fix(MemBlock): fix stuck on misalign vector store (#6337)

src/main/scala/xiangshan/mem/lsqueue/StoreMisalignBuffer.scala
@@ -224,11 +224,11 @@ class StoreMisalignBuffer(implicit p: Parameters) extends XSModule
   val sameUop = io.sqControl.toStoreMisalignBuffer.uop.robIdx === req.uop.robIdx &&
     io.sqControl.toStoreMisalignBuffer.uop.uopIdx === req.uop.uopIdx
 
-  io.sqControl.toStoreQueue.crossPageWithHit := sameSqPtr && isCrossPage
-  io.sqControl.toStoreQueue.crossPageCanDeq := !isCrossPage || bufferState === s_block
+  io.sqControl.toStoreQueue.crossPageWithHit := sameSqPtr && isCrossPage && req_valid
+  io.sqControl.toStoreQueue.crossPageCanDeq := bufferState === s_block
   io.sqControl.toStoreQueue.paddr := Cat(splitStoreResp(1).paddr(splitStoreResp(1).paddr.getWidth - 1, 3), 0.U(3.W))
 
-  io.sqControl.toStoreQueue.withSamePtr := sameUop && req.isvec && robMatch && isCrossPage
+  io.sqControl.toStoreQueue.withSamePtr := sameUop && req.isvec && robMatch && isCrossPage && bufferState === s_block
 
   //state transition
   switch(bufferState) {

OpenXiangShan/XSAI

Adapt SMB handoff state guards and replayed-store misalignment reporting to the XSAI whole-UOP replay protocol.
(cherry picked from commit 9ee7b3352c0959570da5ec820613a0cdd6be11cf)

src/main/scala/xiangshan/mem/lsqueue/StoreMisalignBuffer.scala
@@ -228,11 +228,11 @@ class StoreMisalignBuffer(implicit p: Parameters) extends XSModule
   val sameUop = io.sqControl.toStoreMisalignBuffer.uop.robIdx === req.uop.robIdx &&
     io.sqControl.toStoreMisalignBuffer.uop.uopIdx === req.uop.uopIdx
 
-  io.sqControl.toStoreQueue.crossPageWithHit := sameSqPtr && isCrossPage
-  io.sqControl.toStoreQueue.crossPageCanDeq := !isCrossPage || bufferState === s_block
+  io.sqControl.toStoreQueue.crossPageWithHit := sameSqPtr && isCrossPage && req_valid
+  io.sqControl.toStoreQueue.crossPageCanDeq := bufferState === s_block
   io.sqControl.toStoreQueue.paddr := Cat(splitStoreResp(1).paddr(splitStoreResp(1).paddr.getWidth - 1, 3), 0.U(3.W))
 
-  io.sqControl.toStoreQueue.withSameUop := sameSqPtr && sameUop && req.isvec && robMatch && isCrossPage
+  io.sqControl.toStoreQueue.withSameUop := sameUop && req.isvec && robMatch && isCrossPage && bufferState === s_block
 
   //state transition
   switch(bufferState) {