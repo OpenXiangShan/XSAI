OpenXiangShan/XiangShan

fix(MemBlock): misalign vec elements within a uop are strictly ordered (#6323)
Because cross-page unaligned accesses need to be stored in the
StoreMisalignBuffer, the MergeBuffer cannot commit the entire uop.
Therefore, we need to ensure that before any unaligned element within
the uop is executed, there are no other elements left unexecuted.

In the previous related fix, I incorrectly implemented the replay logic,
and this is corrected here.

src/main/scala/xiangshan/mem/lsqueue/StoreQueue.scala
@@ -1471,7 +1471,7 @@ class StoreQueue(implicit p: Parameters) extends XSModule
 
   // For vector, when there is a store across pages with the same uop in storeMisalignBuffer, storequeue needs to mark this item as committed.
   // TODO FIXME Can vecMbCommit be removed?
-  when(io.maControl.toStoreQueue.withSamePtr && allvalid(rdataPtrExt(0).value)) {
+  when(io.maControl.toStoreQueue.withSamePtr) {
     vecMbCommit(rdataPtrExt(0).value) := true.B
   }
 

OpenXiangShan/XSAI

Port the StoreQueue vecMbCommit release condition while retaining the XSAI whole-UOP replay protocol.
(cherry picked from commit 2cac7a0df6877408e436cc0501cc02a2e94ad976)

src/main/scala/xiangshan/mem/lsqueue/StoreQueue.scala
@@ -1476,7 +1476,7 @@ class StoreQueue(implicit p: Parameters) extends XSModule
 
   // For vector, when there is a store across pages with the same uop in storeMisalignBuffer, storequeue needs to mark this item as committed.
   // TODO FIXME Can vecMbCommit be removed?
-  when(io.maControl.toStoreQueue.withSameUop && allvalid(rdataPtrExt(0).value)) {
+  when(io.maControl.toStoreQueue.withSameUop) {
     vecMbCommit(rdataPtrExt(0).value) := true.B
   }
 